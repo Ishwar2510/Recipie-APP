b9fc271953d3bdb57a6819b3b9f3c38a998e207f
ishwar kumar <ishwar2510@gmail.com>
2022-09-17 19:48:17 +0530

    App created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Recipie from "./components/recipie";
+import {Fallback} from "./components/Fallback";
 function App() {
+  
+  const [apicounter,setapicounter]=useState(0);
+  const APP_KEY = "b94ffb5737d54526a631f10df9aac84e";
+  const [query, setQuery] = useState("pasta");
+  const [data, setData] = useState(Fallback);
+  const [input, setInput] = useState("");
+  let url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${APP_KEY}&query=${query}&addRecipeInformation=true`;
+
+  useEffect(() => {
+    if (query == null) {
+      return;
+    }
+
+    apicall();
+  }, [query]);
+
+  async function apicall() {
+    setapicounter(prev=>prev+1);
+    console.log(apicounter)
+    try {
+      let response = await fetch(url);
+      let receivedData = await response.json();
+      if (receivedData.code == 402) {
+        alert(receivedData.message);
+        setData(Fallback)
+      } else {
+        if (receivedData.results.length > 0) {
+          console.log(receivedData.results);
+          setData(receivedData.results);
+          
+        }
+      }
+     
+    } catch (error) {
+      setData(Fallback)
+      console.log("this is an error");
+      console.log(error);
+    }
+  }
+
+  function formSubmit(e) {
+    e.preventDefault();
+    setQuery(input);
+  }
+  function inputQuery(e) {
+    setInput(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Recipe_App" onSubmit={formSubmit}>
+      <h1> LET COOK SOMETHING</h1>
+      <div className="search_form">
+        <input
+          className="search_bar"
+          type="text"
+          value={input}
+          onChange={inputQuery}
+          placeholder="Enter Item to Search For"
+        ></input>
+        <button className="search_button" onClick={formSubmit}>Search</button>
+      </div>
+      {/* <h1>{query.toUpperCase()}</h1> */}
+      <div className="recepiesConatiner">
+        {data.map((e) => {
+          return <Recipie key={e.id} title={e.title} image={e.image} summary={e.summary} dishTypes={e.dishTypes} serving={e.servings} price={e.pricePerServing} instruction={e.analyzedInstructions}/>;
+        })}
+      </div>
+        <footer>
+          <h6>A recipe has no soul . You as the cook, must bring soul to the recipie</h6>
+          <p>THOMAS KELLER</p>
+        </footer>
     </div>
   );
 }
