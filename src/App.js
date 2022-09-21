99e3d048485cb7ea10eade51797e7fd54ceae991
ishwar kumar <ishwar2510@gmail.com>
2022-09-21 05:41:31 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -8,19 +8,22 @@ function App() {
   const [query, setQuery] = useState("pasta");
   const [data, setData] = useState(Fallback);
   const [input, setInput] = useState("");
+  const [loader,setLoader]=useState(false);
+  
   let url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${APP_KEY}&query=${query}&addRecipeInformation=true`;
 
   useEffect(() => {
     if (query == null) {
       return;
     }
-
+    setLoader(true)
     apicall();
   }, [query]);
 
   async function apicall() {
     setapicounter((prev) => prev + 1);
-    console.log(apicounter);
+    // console.log(apicounter);
+    // seterror(false);
     try {
       let response = await fetch(url);
       let receivedData = await response.json();
@@ -31,17 +34,21 @@ function App() {
         if (receivedData.results.length > 0) {
           console.log(receivedData.results);
           setData(receivedData.results);
+          setLoader(false);
         }
       }
     } catch (error) {
       setData(Fallback);
       console.log("this is an error");
-      console.log(error);
+      // console.log(error);
+      setLoader(false);
+      
     }
   }
 
   function formSubmit(e) {
     e.preventDefault();
+    
     setQuery(input);
   }
   function inputQuery(e) {
@@ -63,7 +70,8 @@ function App() {
           Search
         </button>
       </div>
-      {/* <h1>{query.toUpperCase()}</h1> */}
+      {(loader)?<h1>LOADING......</h1>:""}
+      
       <div className="recepiesConatiner">
         {data.map((e) => {
           return (
